c46dddafa2f23ac6f50816fdaafa2e8466c5f4ef
shinkuraj@curriculumindia.com <shinkuraj@curriculumindia.com>
2022-11-18 11:09:34 +0530

    Launch Settings

--- UI/CurrencyExchange.UI/CurrencyExchange.UI/Pages/Index.razor.cs
+++ UI/CurrencyExchange.UI/CurrencyExchange.UI/Pages/Index.razor.cs
@@ -19,45 +19,52 @@ namespace CurrencyExchange.UI.Pages
 
         decimal? LastSevenDaysGrowth;
 
+        decimal? TodayCurrencyRate;
+
         protected async override Task OnInitializedAsync()
         {
 
             currencylist = await currencyservice.GetAllCurrency();
-
             this.StateHasChanged();
         }
 
+        #region DropDown Currency Changing Event
         private async void CurrencySelectionChange(ChangeEventArgs args)
         {
             if (!string.IsNullOrEmpty(args.Value.ToString()))
             {
-
+                // Get Currency Exchange Rate
                 string Currency = args.Value.ToString();
-                ExchangeRate = await currencyservice.GetExchangeRateByCurrencyName(Currency);
 
+                // Call API and Get Last 7 days Exchange  Rate by Currency
+                ExchangeRate = await currencyservice.GetExchangeRateByCurrencyName(Currency);
                 ExchangeRate = ExchangeRate.OrderBy(x => x.CurrencyDate).ToList();
 
+
+                // find out the last 7 Days Growth 
                 var minimumDate = ExchangeRate.Min(x => x.CurrencyDate);
                 var MaximumDate = ExchangeRate.Max(x => x.CurrencyDate);
 
                 decimal? todayRate=ExchangeRate.First(x=> x.CurrencyDate==MaximumDate).CurrencyRate;
                 decimal? LastseventhDayRate=ExchangeRate.First(x=> x.CurrencyDate== minimumDate).CurrencyRate;
-
                 decimal? diffrence = todayRate - LastseventhDayRate;
-
                 if (diffrence.HasValue)
                 {
                     LastSevenDaysGrowth = (diffrence / todayRate) * 100;
                 }
 
+                // Today Currency rate
+                TodayCurrencyRate = ExchangeRate.FirstOrDefault(x => x.CurrencyDate == MaximumDate).CurrencyRate;
+
+                // Create the Chart
                 List<string?> labels = ExchangeRate.Select(x => x.CurrencyDate.ToString("yyyy-MM-dd")).ToList();
                 List<decimal?> rates = ExchangeRate.Select(x => x.CurrencyRate).ToList();
-
                 await js.InvokeVoidAsync("CreateChart", labels, rates, Currency);
                 this.StateHasChanged();
             }
         }
-        }
-
+        #endregion
     }
+
+}
 
